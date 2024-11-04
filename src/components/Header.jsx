c1fed48872d06128d9caf3ab8659007c9940bad5
Seonghwan Sim <hoanshim01@handong.ac.kr>
2024-11-04 19:52:00 +0900

    몰라

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import { useNavigate } from "react-router-dom";
 
 import "../style/header.css";
+import Minimap_icon from "../image/minimap.svg";
+import Battery_icon from "../image/battery.svg";
 
 export default function Header() {
   const navigate = useNavigate();
@@ -20,10 +22,14 @@ export default function Header() {
           홈
         </div>
         <div className="header_icon" onClick={toMiniMap}>
+          <img src={Minimap_icon} alt="minimap" style={{ height: "20px" }} />
           미니맵
         </div>
       </div>
-      <div className="header_icon">배터리</div>
+      <div className="header_icon">
+        <img src={Battery_icon} alt="battery" style={{ height: "20px" }} />
+        배터리
+      </div>
     </div>
   );
 }
